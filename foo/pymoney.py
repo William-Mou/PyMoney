29a43b747a55cf801faf2daefa8b06656b1a31e4
William <william.mou1024@gmail.com>
2020-10-11 02:44:11 +0800

    Finished HW checkpoint 4 plus

--- foo/pymoney.py
+++ foo/pymoney.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 # PROJECT_LINK : https://github.com/William-Mou/PyMoney
-
+item_list = []
 dollar = input("How much money do you have? ")
-add_item = input(
-    "Add an expense or income record with description and amount:\n")
-item, money = add_item.split()
-money, dollar = map(int, [money, dollar])
-dollar += money
-print("Now you have " + str(dollar) + " dollars.")
+print("Add an expense or income record with description and amount:")
+while True:
+    add_item = input()
+    if add_item == "q":
+        break
+    item, money = add_item.split()
+    item_list.append([item, money])
+    money, dollar = map(int, [money, dollar])
+    dollar += money
+
+print("Now you have " + str(dollar) + " dollars.\nHere is your list:")
+for item_i in item_list:
+    print(item_i[0], item_i[1])
